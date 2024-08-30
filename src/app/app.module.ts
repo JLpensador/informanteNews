eb4cbe9378dfe86cae379f1308808dc36eb7ff9c
JLpensador <joaoluizborgesmorais@gmail.com>
2024-08-30 15:35:24 -0300

    Fix: configurar HttpClient para usar fetch API e corrigir alerta de performance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 import { NewsapiservicesService } from './service/newsapiservices.service';
 import { TopheadingComponent } from './shared/topheading/topheading.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, TopheadingComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [provideClientHydration(), NewsapiservicesService],
+  providers: [
+    provideClientHydration(),
+    NewsapiservicesService,
+    provideHttpClient(withFetch()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
